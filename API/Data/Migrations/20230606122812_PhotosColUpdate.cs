436a8766b91f71b038dada1fda850806e99c8897
Sanket <sanketthale999@gmail.com>
2023-06-06 22:51:06 +0530

    Extended APIs, added mapper, Database modified

---
+++ API/Data/Migrations/20230606122812_PhotosColUpdate.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace API.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class PhotosColUpdate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "isMain",
+                table: "Photos",
+                newName: "IsMain");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "IsMain",
+                table: "Photos",
+                newName: "isMain");
+        }
+    }
+}
