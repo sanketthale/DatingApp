fe600e8ffe5bcda94f57d9ce0c0087c6cada8c2a
Sanket <sanketthale999@gmail.com>
2024-04-20 00:09:03 +0530

    Implemented .net identity, role management, policy based authorization.

--- client/src/app/members/member-list/member-list.component.ts
+++ client/src/app/members/member-list/member-list.component.ts
@@ -38,7 +38,7 @@ export class MemberListComponent implements OnInit {
     this.loadMembers();
   }
 
-  loadMembers() {
+  loadMembers():void {
     if(this.userParams){
       this.memberService.setUserParams(this.userParams);
       this.memberService.getMembers(this.userParams).subscribe({
